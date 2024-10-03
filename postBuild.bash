8ba9f85a74204129b9053d3db0ee2f5c067e7642
Qasim Khan <qasimkhan3.124@gmail.com>
2024-10-03 13:02:45 +0200

    Project Spec Modified. Modified postBuild.bash
    - Updated webapp_options in app 'Chatbot'
    - Modified files: postBuild.bash

--- postBuild.bash
+++ postBuild.bash
@@ -3,5 +3,5 @@
 # after all system packages and programming language specific package have been installed.
 #
 # Note: This file may be removed if you don't need to use it
-curl -fsSL https://ollama.com/install.sh | sh
-python3 -c "import os; from sentence_transformers import SentenceTransformer; from transformers import AutoModelForMaskedLM, AutoTokenizer; model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2'); tokenizer = AutoTokenizer.from_pretrained('prithivida/Splade_PP_en_v1'); model = AutoModelForMaskedLM.from_pretrained('prithivida/Splade_PP_en_v1')"
+# curl -fsSL https://ollama.com/install.sh | sh
+python3 -c "import os; from qdrant_client import QdrantClient; client = QdrantClient(url='$QDRANT_URL', api_key='$QDRANT_API_KEY'); client.set_model('sentence-transformers/all-MiniLM-L6-v2'); client.set_sparse_model('prithivida/Splade_PP_en_v1')"
