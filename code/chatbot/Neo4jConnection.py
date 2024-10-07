968e41733329aec3b160386e305a0213fc5c4afc
Qasim Khan <qasimkhan3.124@gmail.com>
2024-10-07 14:00:17 +0000

    remove driver as property

--- code/chatbot/Neo4jConnection.py
+++ code/chatbot/Neo4jConnection.py
@@ -5,24 +5,20 @@ class Neo4jConnection:
         self.__uri = uri
         self.__user = user
         self.__password = password
-        self.__driver = None
         self.__db = db
         try:
-            self.__driver = GraphDatabase.driver(self.__uri, auth=(self.__user, self.__password))
-            self.__driver.verify_connectivity() 
+            with GraphDatabase.driver(self.__uri, auth=(self.__user, self.__password)) as driver:
+                driver.verify_connectivity()       
         except Exception as e:
             print("Failed to create the driver:", e)
-
-    def close(self):
-        if self.__driver:
-            self.__driver.close()
 
     def query(self, query, **parameters):
         response = None
         success = False
         try:
-            with self.__driver.session(database=self.__db) as session:
-                response = list(session.run(query, **parameters))
+            with GraphDatabase.driver(self.__uri, auth=(self.__user, self.__password)) as driver:
+                with driver.session(database=self.__db) as session:
+                    response = list(session.run(query, **parameters))
         except Exception as e:
             print("Query failed:", e)
         else:
@@ -32,15 +28,16 @@ class Neo4jConnection:
     def bulk_query(self, queries, **parameters):
         response = None
         try:
-            with self.__driver.session(database=self.__db) as session:
-                response = []
-                # Use transaction to execute all queries within one transaction block
-                with session.begin_transaction() as txn:
-                    for query in queries:
-                        result = txn.run(query, parameters)
-                        response.append(list(result))
-                    # Commit the transaction after all queries are run
-                    txn.commit()
+            with GraphDatabase.driver(self.__uri, auth=(self.__user, self.__password)) as driver:
+                with driver.session(database=self.__db) as session:
+                    response = []
+                    # Use transaction to execute all queries within one transaction block
+                    with session.begin_transaction() as txn:
+                        for query in queries:
+                            result = txn.run(query, parameters)
+                            response.append(list(result))
+                        # Commit the transaction after all queries are run
+                        txn.commit()
         except Exception as e:
             print("Query failed:", e)
         return response
@@ -50,3 +47,11 @@ class Neo4jConnection:
         query_result, success = self.query(cypher)
         if success:
             return [dict(record) for record in query_result]
+
+if __name__ == "__main__":
+    import os
+    # Test the connection
+    uri = os.getenv("NEO4J_URI")
+    AUTH = ("neo4j", os.getenv("NEO4J_PASSWORD"))
+    driver = Neo4jConnection(uri, *AUTH, "neo4j")
+    
